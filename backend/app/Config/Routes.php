a6c32bd278d6d3ba3407e40fea55d2c725392dac
sergioh665 <sergioh665@gmail.com>
2024-12-18 00:11:31 -0300

    🔧CHORE: adcionando endpoints

--- backend/app/Config/Routes.php
+++ backend/app/Config/Routes.php
@@ -6,3 +6,7 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('tarefas', 'TarefaController::index');
+$routes->post('tarefas', 'TarefaController::create');
+$routes->put('tarefas/(:num)', 'TarefaController::update/$1');
+$routes->delete('tarefas/(:num)', 'TarefaController::delete/$1');
